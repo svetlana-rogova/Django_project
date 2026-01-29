ec2806e6255916e8a0d95a2aaf3ca1dc3ac56dec
Светлана Рогова <mifistofiya@gmail.com>
2026-01-29 23:21:33 +0300

    fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'catalog'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('', include('catalog.urls', namespace='catalog')),
 ]
