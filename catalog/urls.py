910d68646c643c3f91c3e7725b5f9b00f8f1fc7d
Светлана Рогова <mifistofiya@gmail.com>
2026-03-03 23:58:18 +0300

    add update and delete

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomeListView, ContactsView, InfoProductDetailView, AddInfoCreateView
+from .views import HomeListView, ContactsView, InfoProductDetailView, AddInfoCreateView, InfoProductDeleteView, AddInfoUpdateView
 
 app_name = 'catalog'
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contact'),
     path('info_product/<int:pk>', InfoProductDetailView.as_view(), name='info_product'),
-    path('add_info/', AddInfoCreateView.as_view(), name='add_info')
+    path('add_info/', AddInfoCreateView.as_view(), name='add_info'),
+    path('delete/<int:pk>', InfoProductDeleteView.as_view(), name='delete'),
+    path('update/<int:pk>', AddInfoUpdateView.as_view(), name='update'),
 ]
