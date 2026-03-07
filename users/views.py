1712a02f86a53959763482801960cd87df6f7651
Светлана Рогова <mifistofiya@gmail.com>
2026-03-08 00:08:36 +0300

    Add view register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
+from django.core.mail import send_mail
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    template_name = 'users/register.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        user = form.save()
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = 'Добро пожаловать'
+        massage = 'Спасибо, что зарегистрировались на нашем сайте'
+        from_email = 'mifistofiya@gmail.com'
+        recipient_list = [user_email]
+        send_mail(subject, massage, from_email, recipient_list)
