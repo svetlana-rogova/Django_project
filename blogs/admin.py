4d16a5e9ba152e31caedb95f05fc49b9af768f55
Светлана Рогова <mifistofiya@gmail.com>
2026-03-11 23:53:56 +0300

    Add admin for blogs

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blogs.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('heading', 'content', 'created_at',)
+    list_filter = ('heading',)
+    search_fields = ('heading',)
